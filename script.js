e4f13e19176ec465d6865edc0c270cda8377f6c4
CJ Hauser <christopher.j.hauser2025@outlook.com>
2025-03-03 11:25:20 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Object to hold audio files
+const sounds = {
+    sound1: new Audio('sounds/xboxearrape.mp3'),
+    sound2: new Audio('sounds/thomasthetrain.mp3'),
+    sound3: new Audio('sounds/gamecube.mp3')
+};
+
+// Add event listeners to buttons
+document.querySelectorAll('.sound-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const sound = button.getAttribute('data-sound');
+        
+        // Stop all sounds before playing a new one
+        Object.values(sounds).forEach(audio => {
+            audio.pause();
+            audio.currentTime = 0;
+        });
+
+        // Play selected sound
+        sounds[sound].play();
+    });
+});
